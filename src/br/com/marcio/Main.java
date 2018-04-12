580c175b73c1837c21b1a1ca22e5ef728d93109e
marciomarquesti <marciomarquesdiasti@gmail.com>
2018-04-12 12:55:36 -0300

    Primeira Versão

---
+++ src/br/com/marcio/Main.java
@@ -0,0 +1,10 @@
+package br.com.marcio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Automovel gol = new Automovel("Gol 1.0", 1000);
+		System.out.println(gol.toString());
+	}
+
+}
---
+++ src/br/com/marcio/Motor.java
@@ -0,0 +1,46 @@
+package br.com.marcio;
+
+public class Motor {  
+  
+    /* 
+     * Atributos. 
+     */  
+    private int potencia;  
+  
+    /* 
+     * Método construtor passando a potência do objeto motor como parâmetro 
+     */  
+    public Motor(int potencia) {  
+        this();  
+        this.potencia = potencia;  
+    }  
+  
+    /* 
+     * Método construtor padrão 
+     */  
+    public Motor() {  
+        super();  
+    }  
+  
+    /* 
+     * Métodos Getters and Setters 
+     */  
+    public int getPotencia() {  
+        return this.potencia;  
+    }  
+  
+    public void setPotencia(int potencia) {  
+        this.potencia = potencia;  
+    }  
+  
+    /* 
+     * Método toString() para impressão em formato String do objeto Motor 
+     */  
+    @Override  
+    public String toString() {  
+        StringBuilder builder = new StringBuilder();  
+        builder.append(potencia);  
+        return builder.toString();  
+    }  
+  
+}  
